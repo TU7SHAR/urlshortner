0b6bd775743fbed73f7e43c461e38b23accc3e54
TU7SHAR <brahmin00011@gmail.com>
2025-07-08 10:47:50 +0530

    url shortner uploading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UrlShortenerForm from "./components/UrlShortenerForm";
+import StatsPage from "./components/StatsPage";
+import UrlRedirectHandler from "./components/UrlRedirectHandler"; // Create this soon
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<UrlShortenerForm />} />
+        <Route path="/stats" element={<StatsPage />} />
+        <Route path="/:shortcode" element={<UrlRedirectHandler />} />
+      </Routes>
+    </Router>
   );
 }
 
